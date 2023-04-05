edf621846e385573d3019e3d7384a667f54a4e97
Yuanzi <yuanzigsa@gmail.com>
2023-04-05 21:52:16 +0800

    多进程ping主机

--- ping.py
+++ ping.py
@@ -1,27 +1,36 @@
 import subprocess
-from multiprocessing import Pool
+import multiprocessing
+import ipaddress
 
-hosts = ["58.52.173.2", "192.168.1.2", "192.168.1.3", "192.168.1.4", "192.168.1.5"]
 
 def ping(host):
-    """
-    Ping the given host and return the result
-    """
-    result = subprocess.run(['ping', '-c', '1', host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    """Ping 指定主机，并返回结果"""
+    result = subprocess.run(['ping', '-n', '1', '-w', '1000', str(host)], capture_output=True)
     if result.returncode == 0:
         return (host, True)
     else:
         return (host, False)
 
+
 if __name__ == '__main__':
-    # Create a process pool with 5 workers
-    with Pool(processes=5) as pool:
-        # Ping all hosts in parallel
+    ip = input('请输入 IP 地址：')
+    mask = input('请输入子网掩码：')
+
+    # 计算子网地址
+    net = ipaddress.IPv4Network(f'{ip}/{mask}', strict=False)
+    subnet = str(net.network_address)
+
+    # 生成子网内所有地址
+    hosts = [str(host) for host in net.hosts()]
+
+    # 并行 ping 操作
+    with multiprocessing.Pool(processes=len(hosts)) as pool:
         results = pool.map(ping, hosts)
 
-    # Print the results
-    for host, success in results:
-        if success:
-            print(f"{host} is up")
-        else:
-            print(f"{host} is down")
+    # 按是否可达分别存储地址
+    reachable_hosts = [host for host, reachable in results if reachable]
+    unreachable_hosts = [host for host, reachable in results if not reachable]
+
+    # 显示可达地址和不可达地址
+    print(f'可达地址：{", ".join(reachable_hosts)}')
+    print(f'不可达地址：{", ".join(unreachable_hosts)}')
