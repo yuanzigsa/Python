8a4336aeaafcddc73c838bc473fd179cb23d533b
Yuanzi <yuanzigsa@gmail.com>
2023-04-07 23:00:54 +0800

    range

---
+++ number.py
@@ -0,0 +1,15 @@
+r = range(10)
+print(r)
+print(list(r))
+
+r = range(1,10) #起止位
+print(list(r))
+
+r = range(1,10,2) #起止位 步长
+print(list(r))
+
+'''判断整数是否在序列中'''
+
+print(10 in r)
+print(0 in r)
+print(1 in r)
